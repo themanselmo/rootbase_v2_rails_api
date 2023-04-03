98b0966fef3fc274251ce4376972d5dc2c280bb7
max anselmo <itmanselmo@gmail.com>
2023-04-02 21:07:03 -0400

    add handling for org tokens

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,19 +2,22 @@ class SessionsController < ApplicationController
     skip_before_action :authorize, only: [:create]
 
     def create
-        user = User.find_by(name: params[:email_address])
+        user = User.find_by(email_address: params[:email_address])
         
         if user&.authenticate(params[:password])
             session[:user_token] = user.auth_token
-            
+            session[:org_token] = user.organization.auth_token
+
             render json: user, status: :created
         else
-            render json: { errors: ["Invalid username or password"] }, status: :unauthorized
+            render json: { errors: ["Invalid email or password"] }, status: :unauthorized
         end
     end
 
     def destroy 
         session.delete :user_token
+        session.delete :org_token
+
         head :no_content
     end
 
