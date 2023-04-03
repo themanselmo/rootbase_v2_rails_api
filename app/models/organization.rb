ec3ba70d371e582bca758d48143a3b377d254560
max anselmo <itmanselmo@gmail.com>
2023-04-02 21:08:29 -0400

    add method for setting up first user on a new account

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,6 +1,12 @@
 class Organization < ApplicationRecord
-    has_many :users
-
+    has_many :users, dependent: :destroy
+    has_one :owner, -> { where role: 'owner' }, class_name: 'User'
+    
     has_secure_token :auth_token
+
+    def initialize_owner(user_params)
+        # create main user, defined by the attribute role: 'owner'
+        users.create(**user_params, role: 'owner')
+    end
 
 end
